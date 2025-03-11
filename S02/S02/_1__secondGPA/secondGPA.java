4891ad99dbe022faddc5eeff3910bc537ce7f0f1
meyssamh <meyssamh@web.de>
2025-03-11 20:47:30 +0330

    added 1 to s02/s02

---
+++ S02/S02/_1__secondGPA/secondGPA.java
@@ -0,0 +1,55 @@
+package _1__secondGPA;
+
+import java.util.Scanner;
+
+// Write a program that reads the student number and
+// GPA of N students from the input, finds the student
+// with the second highest GPA, and outputs it.
+
+public class secondGPA {
+    public static void main(String[] args) {
+        int firstStudent = -1;
+        int secondStudent = -1;
+
+        float firstGpa = -1;
+        float secondGpa = -1;
+
+        System.out.println("Please enter the number of students:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int students = scanner.nextInt();
+
+        if (students < 2) {
+            System.out.println("Please enter a number greater than 1!");
+
+            scanner.close();
+            
+            return;
+        }
+
+        for(int i = 1; i <= students; i++) {
+            System.out.println("Please enter the student id number:");
+
+            int id = scanner.nextInt();
+
+            System.out.println("Please enter the GPA of the student:");
+
+            float gpa = scanner.nextFloat();
+
+            if (gpa > firstGpa) {
+                secondStudent = firstStudent;
+                secondGpa = firstGpa;
+                firstGpa = gpa;
+                firstStudent = id;
+            } else if (gpa > secondGpa) {
+                secondGpa = gpa;
+                secondStudent = id;
+            }
+        }
+
+        scanner.close();
+
+        System.out.println("The student with the second highest GPA hasid number of " + secondStudent + " and GPA of " + secondGpa + ".");
+    }
+}
